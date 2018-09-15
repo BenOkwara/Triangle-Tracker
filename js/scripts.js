f21bfa75be83d2297cdcb8631c55747cfbcf3a39
BenOkwara <maranathabenson@gmail.com>
2018-09-15 14:55:52 +0300

    Add a div class element in index.html file

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ function typeOf() {
   var a = document.getElementById("length").value;
   var b = document.getElementById("width").value;
   var c = document.getElementById("height").value;
+  
   console.log(a);
   console.log(b);
   console.log(c);
