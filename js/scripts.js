68f4998b5c94c854154df3db7683428900021ae9
BenOkwara <maranathabenson@gmail.com>
2018-09-15 14:42:52 +0300

    Add console element to scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,6 @@ function typeOf() {
   var a = document.getElementById("length").value;
   var b = document.getElementById("width").value;
   var c = document.getElementById("height").value;
+  console.log(a);
+  console.log(b);
+  console.log(c);
