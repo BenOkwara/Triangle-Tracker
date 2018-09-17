4390f2270448a5a9e717a00dee9d7f0d187ff510
BenOkwara <maranathabenson@gmail.com>
2018-09-17 11:32:01 +0300

    Add the changes made on scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function typeOf() {
   var a = document.getElementById("length").value;
   var b = document.getElementById("width").value;
   var c = document.getElementById("height").value;
-  
+
   console.log(a);
   console.log(b);
   console.log(c);
@@ -14,7 +14,7 @@ function typeOf() {
       alert("This is Isoceles")
     }
     else if ((a+b)<=c || (a+c)<=b || (b+c)<=a) {
-      alert("Oops Aint a Triangle");
+      alert("Oops Ain't an Triangle pass");
     }
     else {
       alert("Am Scalene");
